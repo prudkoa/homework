7f2c09dac345be0bb6bfa41c7111ee171567d4e8
Прудько Артур Анатольевич <aaprudk1@mts.ru>
2021-09-10 02:07:54 +0300

    1

---
+++ homework_01/main.py
@@ -0,0 +1,46 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*n):
+    for num in n:
+       if (str(num).isdigit()):
+          print(num**2)
+
+power_numbers(1, 2, 5, 7, 4.4)
+
+print("__________________________________")
+def is_prime(*n):
+    for num in n:
+        if(num==3):
+            print(num)
+        if(num==2):
+            print(num)
+        if (num != 1 and num % 2 != 0 ):
+          if (num != 1 and num % 3 !=0 ):
+            print(num)
+
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+def filter_numbers(n, filter):
+    if filter == ODD:
+        for num in n:
+            if num % 2 ==1:
+                print(num)
+    if filter == EVEN:
+        for num in n:
+            if num % 2 != 1:
+                print(num)
+    if filter == PRIME:
+        for num in n:
+                is_prime(num)
+filter_numbers([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19], PRIME)
+
+
